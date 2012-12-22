61dbd3d3aa2b068bf47919124d2d7b864abb7406
Nicolas Viennot <nicolas@viennot.biz>
2012-12-22 05:25:28 -0500

    Database create retries

--- lib/no_brainer/query.rb
+++ lib/no_brainer/query.rb
@@ -1,16 +1,21 @@
 module NoBrainer::Query
   def self.run(options={}, &block)
-    NoBrainer.connection.run(options, &block)
-  rescue RuntimeError => e
-    if e.message =~ /`FIND_TABLE (.+)`: No entry with that name/
-      # TODO Lookup the Model, and get the options for the primary key
-      NoBrainer.table_create $1
-      retry
-    elsif e.message =~ /`FIND_DB (.+)`: No entry with that name/
-      NoBrainer.db_create $1
-      retry
-    else
-      raise e
+    db_find_retries = 0
+    begin
+      NoBrainer.connection.run(options, &block)
+    rescue RuntimeError => e
+      if e.message =~ /`FIND_TABLE (.+)`: No entry with that name/
+        # TODO Lookup the Model, and get the options for the primary key
+        NoBrainer.table_create $1
+        retry
+      elsif e.message =~ /`FIND_DB (.+)`: No entry with that name/
+        # RethinkDB may return an FIND_DB not found immediately
+        # after having created the new database, Be patient.
+        NoBrainer.db_create $1 if db_find_retries == 0
+        retry if (db_find_retries += 1) < 10
+      else
+        raise e
+      end
     end
   end
 end
