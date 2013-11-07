abf8a635eb398be0952fa3bfa6f950be1f10ebf6
Nicolas Viennot <nicolas@viennot.biz>
2013-11-06 21:50:15 -0500

    Refactored where() regexp
    Closes #22

--- lib/no_brainer/selection/where.rb
+++ lib/no_brainer/selection/where.rb
@@ -1,19 +1,31 @@
 module NoBrainer::Selection::Where
-  def filter(*args, &block)
-    filtered = nil
-    options = args.last
-    options.each do |key, value|
-      if value.is_a?(Regexp)
-        value = options.delete(key)
-        filtered = match(key, value).filter(options)
-      end
-    end if options
-    chain (filtered || query).filter(*args, &block)
-  end
-  alias where filter
+  def where(*args, &block)
+    options = args.extract_options!.dup
+    regexp_filters = extract_regexp!(options)
+    default_option = options.select { |k,v| k == :default }
 
-  def match(field, regex)
-    regex_string = regex.inspect[1..-2]
-    chain query.filter { |row| row[field].match(regex_string) }
+    filtered_query = query.filter(*args, options, &block)
+
+    if !regexp_filters.empty?
+      filtered_query = filtered_query.filter(default_option) do |doc|
+        regexp_filters.map    { |field, regexp| doc[field].match(regexp) }
+                      .reduce { |a,b| a & b }
+      end
+    end
+
+    chain filtered_query
+  end
+
+  private
+
+  def extract_regexp!(options)
+    regexp_filters = {}
+    options.each do |k,v|
+      if v.is_a?(Regexp)
+        options.delete(k)
+        regexp_filters[k] = v.inspect[1..-2]
+      end
+    end
+    regexp_filters
   end
 end
