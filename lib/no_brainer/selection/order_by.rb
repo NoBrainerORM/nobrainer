83443633c908e9f7a2913125b2f8231879377abd
Ess <pooster@gmail.com>
2013-11-29 14:38:24 -0500

    Refactored smells out of selection/order_by.rb
    Smells reported by reek:
    
    Before:
    
     3: NoBrainer::Selection::OrderBy#order_by calls rules[0] twice
        (DuplicateMethodCall)
     5: NoBrainer::Selection::OrderBy#order_by has the variable name 'k'
        (UncommunicativeVariableName)
     5: NoBrainer::Selection::OrderBy#order_by has the variable name 'v'
        (UncommunicativeVariableName)
    
    After:
    
    Nothing to report
    
    Notes:
    
    Fairly trivial change to the uncommunicative variable names and a
    local variable to track rules[0]

--- lib/no_brainer/selection/order_by.rb
+++ lib/no_brainer/selection/order_by.rb
@@ -1,12 +1,13 @@
 module NoBrainer::Selection::OrderBy
   def order_by(*rules)
-    if rules[0].is_a? Hash
+    rule = rules[0]
+    if rule.is_a? Hash
       # Exploiting the fact that Hashes are now ordered
-      rules = rules[0].map do |k,v|
-        case v
-        when :asc  then RethinkDB::RQL.new.asc(k)
-        when :desc then RethinkDB::RQL.new.desc(k)
-        else raise "please pass :asc or :desc, not #{v}"
+      rules = rule.map do |key,value|
+        case value
+        when :asc  then RethinkDB::RQL.new.asc(key)
+        when :desc then RethinkDB::RQL.new.desc(key)
+        else raise "please pass :asc or :desc, not #{value}"
         end
       end
     end
