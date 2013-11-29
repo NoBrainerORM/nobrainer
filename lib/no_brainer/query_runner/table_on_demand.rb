09e1593fe62f741052b1631f551703d34dfe42a5
Ess <pooster@gmail.com>
2013-11-29 14:29:12 -0500

    Refactored smells out of query_runner/table_on_demand.rb
    Smells reported by reek:
    
    Before:
    
     1: NoBrainer::QueryRunner::TableOnDemand has no descriptive comment
        (IrresponsibleModule)
     5: NoBrainer::QueryRunner::TableOnDemand#call has the variable name
        'e' (UncommunicativeVariableName)
    
    After:
    
     1: NoBrainer::QueryRunner::TableOnDemand has no descriptive comment
        (IrresponsibleModule)
    
    Notes:
    
    Just a trivial change (e -> err)

--- lib/no_brainer/query_runner/table_on_demand.rb
+++ lib/no_brainer/query_runner/table_on_demand.rb
@@ -1,12 +1,12 @@
 class NoBrainer::QueryRunner::TableOnDemand < NoBrainer::QueryRunner::Middleware
   def call(env)
     @runner.call(env)
-  rescue RuntimeError => e
-    if e.message =~ /^Table `(.+)` does not exist\.$/
+  rescue RuntimeError => err
+    if err.message =~ /^Table `(.+)` does not exist\.$/
       # TODO Lookup the Model, and get the primary key name
       NoBrainer.table_create $1
       retry
     end
-    raise e
+    raise err
   end
 end
