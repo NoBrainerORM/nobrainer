45ccad644d91d133942d4487899c4feb5abbaa0e
Ess <pooster@gmail.com>
2013-11-29 12:01:04 -0500

    Refactored smells out of document/serialization.rb
    Smells reported by reek:
    
    Before:
    
    33: NoBrainer::Document::Serialization#serializable_hash contains
        iterators nested 2 deep (NestedIterators)
    15: NoBrainer::Document::Serialization#serializable_hash has approx
        11 statements (TooManyStatements)
    33: NoBrainer::Document::Serialization#serializable_hash has the
        variable name 'a' (UncommunicativeVariableName)
    26: NoBrainer::Document::Serialization#serializable_hash has the
        variable name 'n' (UncommunicativeVariableName)
    
    After:
    
    Nothing to report
    
    Notes:
    
    Removing the smells in question without introducing a lot more smells
    required fairly liberal use of Extract Method.
    
    Methods that act on self were extracted into private methods, and
    methods that don't were extracted into class methods.
    
    One caveat is that the serialization module is specified explicitly in
    the private serialization_class method, because I didn't think it would
    be kosher to force the class methods into every class that includes the
    serialization module.

--- lib/no_brainer/document/serialization.rb
+++ lib/no_brainer/document/serialization.rb
@@ -7,35 +7,63 @@ module NoBrainer::Document::Serialization
 
   included { self.include_root_in_json = false }
 
-  # XXX This is a giant copy paste from lib/active_model/serialization.rb
-  # The diff is:
-  # - attribute_names = attributes.keys.sort
-  # + attribute_names = self.class.fields.keys.sort
-  # That's all.
+  # XXX This was a giant copy paste from lib/active_model/serialization.rb
+  # It has been refactored to smell a bit better
   def serializable_hash(options = nil)
     options ||= {}
 
-    attribute_names = self.class.fields.keys.sort
-    if only = options[:only]
-      attribute_names &= Array.wrap(only).map(&:to_s)
-    elsif except = options[:except]
-      attribute_names -= Array.wrap(except).map(&:to_s)
-    end
-
-    hash = {}
-    attribute_names.each { |n| hash[n] = read_attribute_for_serialization(n) }
-
-    method_names = Array.wrap(options[:methods]).select { |n| respond_to?(n) }
-    method_names.each { |n| hash[n] = send(n) }
+    hash = attributes_and_methods_hash(
+      serialization_class.filter_attribute_names(
+        self.class.fields.keys.sort, options),
+      options[:methods])
 
     serializable_add_includes(options) do |association, records, opts|
-      hash[association] = if records.is_a?(Enumerable)
-        records.map { |a| a.serializable_hash(opts) }
-      else
-        records.serializable_hash(opts)
-      end
+      hash[association] = module_klass.associated_hash_value(records, opts)
     end
 
     hash
+  end
+
+  private
+  def serialization_class
+    # This is admittedly a little smelly, but it prevents us having to break
+    # out a new module just for two utility functions (or including those
+    # utility functions directly when included in a document
+    NoBrainer::Document::Serialization
+  end
+
+  def attributes_and_methods_hash(attributes, methods)
+    serialized_attributes(attributes).merge(serialized_methods(methods))
+  end
+
+  def serialized_attributes(attributes)
+    Hash[
+      attributes.map {|name| [name, read_attribute_for_serialization(name)]}
+    ]
+  end
+
+  def serialized_methods(methods)
+    Hash[
+      Array.wrap(methods).select {|name| respond_to?(name)}.map {|name| [name, send(name)]}
+    ]
+  end
+
+  def self.associated_hash_value(records, opts)
+    # This is a utility function to grab the serializable hash of the records
+    # passed in
+    records.is_a?(Enumerable) ?
+      records.map {|record| record.serializable_hash(opts)} :
+      records.serializable_hash(opts)
+  end
+
+  def self.filter_attribute_names(attribute_names, options)
+    # This is a utility function to get us a list of attribute names, possibly
+    # filtered by :only and :except
+    if only = options[:only]
+      return attribute_names & Array.wrap(only).map(&:to_s)
+    elsif except = options[:except]
+      return attribute_names - Array.wrap(except).map(&:to_s)
+    end
+    attribute_names
   end
 end
