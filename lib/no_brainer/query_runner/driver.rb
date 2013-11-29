3db17f0c961729ddfd2c3bcda288b0b5b4332998
Ess <pooster@gmail.com>
2013-11-29 13:36:24 -0500

    Refactored smells out of query_runner/driver.rb
    Smells reported by reek:
    
    Before:
    
     1: NoBrainer::QueryRunner::Driver has no descriptive comment
        (IrresponsibleModule)
     5: NoBrainer::QueryRunner::Driver#call calls NoBrainer.connection
        twice (DuplicateMethodCall)
     4: NoBrainer::QueryRunner::Driver#call calls env[:query] twice
        (DuplicateMethodCall)
     7: NoBrainer::QueryRunner::Driver#call has the variable name 'e'
        (UncommunicativeVariableName)
    
    After:
    
     1: NoBrainer::QueryRunner::Driver has no descriptive comment
        (IrresponsibleModule)
    
    Notes:
    
    Fairly trivial changes again. Extract method and local variables.

--- lib/no_brainer/query_runner/driver.rb
+++ lib/no_brainer/query_runner/driver.rb
@@ -1,10 +1,16 @@
 class NoBrainer::QueryRunner::Driver < NoBrainer::QueryRunner::Middleware
   def call(env)
     # TODO have a logger
-    puts env[:query].inspect if ENV['DEBUG']
-    env[:query].run(NoBrainer.connection, env[:options])
-  rescue NoMethodError => e
-    raise "NoBrainer is not connected to a RethinkDB instance" unless NoBrainer.connection
-    raise e
+    query, options = env.values_at(:query, :options)
+    puts query.inspect if ENV['DEBUG']
+    query.run(connection, options)
+  rescue NoMethodError => err
+    raise "NoBrainer is not connected to a RethinkDB instance" unless connection
+    raise err
+  end
+
+  private
+  def connection
+    NoBrainer.connection
   end
 end
