aeacf61b0736b397242e42dc479fce492e6adde7
Ess <pooster@gmail.com>
2013-11-29 14:26:32 -0500

    Refactored smells out of query_runner/write_error.rb
    Smells reported by reek:
    
    Before:
    
     1: NoBrainer::QueryRunner::WriteError has no descriptive comment
        (IrresponsibleModule)
    15: NoBrainer::QueryRunner::WriteError#call calls e.message twice
        (DuplicateMethodCall)
     8: NoBrainer::QueryRunner::WriteError#call calls is_write_query?(env)
        twice (DuplicateMethodCall)
    13: NoBrainer::QueryRunner::WriteError#call has the variable name
        'e' (UncommunicativeVariableName)
    22: NoBrainer::QueryRunner::WriteError#is_write_query? doesn't depend
        on instance state (UtilityFunction)
    22: NoBrainer::QueryRunner::WriteError#is_write_query? refers to env
        more than self (FeatureEnvy)
    
    After:
    
     1: NoBrainer::QueryRunner::WriteError has no descriptive comment
        (IrresponsibleModule)
    
    Notes:
    
    Broke out a class utility method to normalize error messages

--- lib/no_brainer/query_runner/write_error.rb
+++ lib/no_brainer/query_runner/write_error.rb
@@ -1,25 +1,23 @@
 class NoBrainer::QueryRunner::WriteError < NoBrainer::QueryRunner::Middleware
   def call(env)
+    write_query = self.class.is_write_query?(env)
     @runner.call(env).tap do |result|
       # TODO Fix rethinkdb driver: Their classes Term, Query, Response are
       # not scoped to the RethinkDB module! (that would prevent a user from
       # creating a Response model for example).
 
-      if is_write_query?(env) && (result['errors'].to_i != 0 || result['skipped'].to_i != 0)
+      if write_query && (result['errors'].to_i != 0 || result['skipped'].to_i != 0)
         raise_write_error(env, result['first_error'])
       end
     end
-  rescue RethinkDB::RqlRuntimeError => e
-    raise unless is_write_query?(env)
-
-    error_msg = e.message.split("\nBacktrace").first
-    error_msg = "Non existent document" if e.message =~ /Expected type OBJECT but found NULL/
-    raise_write_error(env, error_msg)
+  rescue RethinkDB::RqlRuntimeError => err
+    raise unless write_query
+    raise_write_error(env, err.message)
   end
 
   private
 
-  def is_write_query?(env)
+  def self.is_write_query?(env)
     env[:query].body.type.in?([Term::TermType::UPDATE,
                                Term::TermType::DELETE,
                                Term::TermType::REPLACE,
@@ -27,8 +25,20 @@ class NoBrainer::QueryRunner::WriteError < NoBrainer::QueryRunner::Middleware
   end
 
   def raise_write_error(env, error_msg)
-    error_msg ||= "Unknown error"
+    error_msg = self.class.normalize_message(error_msg)
     error_msg += "\nQuery was: #{env[:query].inspect[0..1000]}"
     raise NoBrainer::Error::DocumentNotSaved, error_msg
+  end
+
+  def self.normalize_message(message)
+    message.to_s.tap do |msg|
+      if msg.eql?('')
+        return 'Unknown error'
+      elsif msg =~ /Expected type OBJECT but found NULL/
+        return "Non existent document"
+      else
+        return msg.split("\nBacktrace").first
+      end
+    end
   end
 end
