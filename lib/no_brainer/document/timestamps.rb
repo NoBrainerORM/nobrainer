cbc0110fb5bb0367185166ef69facd7cdd88e55e
Nicolas Viennot <nicolas@viennot.biz>
2014-01-07 22:06:05 -0500

    Improve disable_timestamps logic

--- lib/no_brainer/document/timestamps.rb
+++ lib/no_brainer/document/timestamps.rb
@@ -2,15 +2,18 @@ module NoBrainer::Document::Timestamps
   extend ActiveSupport::Concern
 
   included do
+    class_attribute :timestamps_disabled
+
     self.field :created_at, :type => Time
     self.field :updated_at, :type => Time
 
-    before_create { self.created_at = Time.now if self.respond_to?(:created_at=) }
-    before_save   { self.updated_at = Time.now if self.respond_to?(:updated_at=) }
+    before_create { self.created_at = Time.now unless self.timestamps_disabled }
+    before_save   { self.updated_at = Time.now unless self.timestamps_disabled }
   end
 
   module ClassMethods
     def disable_timestamps
+      self.timestamps_disabled = true
       self.remove_field :created_at
       self.remove_field :updated_at
     end
