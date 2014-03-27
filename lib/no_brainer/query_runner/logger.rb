db46686c8eba2359070cf011714896082570badc
Nicolas Viennot <nicolas@viennot.biz>
2014-03-27 17:30:29 -0400

    Logger logs the query even when an exception is going through

--- lib/no_brainer/query_runner/logger.rb
+++ lib/no_brainer/query_runner/logger.rb
@@ -1,24 +1,35 @@
 class NoBrainer::QueryRunner::Logger < NoBrainer::QueryRunner::Middleware
   def call(env)
     start_time = Time.now
-    res = @runner.call(env)
-    if NoBrainer.logger and NoBrainer.logger.debug?
-      duration = Time.now - start_time
-      msg = env[:query].inspect.gsub(/\n/, '').gsub(/ +/, ' ')
+    @runner.call(env).tap { log_query(env, start_time) }
+  rescue Exception => e
+    log_query(env, start_time, e) rescue nil
+    raise e
+  end
 
-      msg = "(#{env[:db_name]}) #{msg}" if env[:db_name]
-      msg = "[#{(duration * 1000.0).round(1)}ms] #{msg}"
+  private
 
-      if NoBrainer::Config.colorize_logger
+  def log_query(env, start_time, exception=nil)
+    return unless NoBrainer.logger.debug?
+
+    duration = Time.now - start_time
+    msg = env[:query].inspect.gsub(/\n/, '').gsub(/ +/, ' ')
+
+    msg = "(#{env[:db_name]}) #{msg}" if env[:db_name]
+    msg = "[#{(duration * 1000.0).round(1)}ms] #{msg}"
+
+    if NoBrainer::Config.colorize_logger
+      if exception
+        msg = "#{msg} \e[0;31m#{exception.class} #{exception.message.split("\n").first}\e[0m"
+      else
         case NoBrainer::Util.rql_type(env[:query])
         when :write      then msg = "\e[1;31m#{msg}\e[0m" # red
         when :read       then msg = "\e[1;32m#{msg}\e[0m" # green
         when :management then msg = "\e[1;33m#{msg}\e[0m" # yellow
         end
       end
-
-      NoBrainer.logger.debug(msg)
     end
-    res
+
+    NoBrainer.logger.debug(msg)
   end
 end
