e18ada0117cf97fda07fbb22450c801829d9b76f
Sharipov Ruslan <sharipov.ru@gmail.com>
2014-04-08 22:37:00 +0700

    include table name information to missing index exception

--- lib/no_brainer/query_runner/missing_index.rb
+++ lib/no_brainer/query_runner/missing_index.rb
@@ -2,8 +2,13 @@ class NoBrainer::QueryRunner::MissingIndex < NoBrainer::QueryRunner::Middleware
   def call(env)
     @runner.call(env)
   rescue RethinkDB::RqlRuntimeError => e
-    if e.message =~ /^Index `(.+)` was not found\.$/
-      raise NoBrainer::Error::MissingIndex.new("Please run \"rake db:update_indexes\" to create the index `#{$1}`\n" +
+
+    index_data = e.message.match /^Index `(?<name>.+)` was not found\.$/
+    table_data = e.message.match /table\(\"(?<name>\S+)\"\)/
+
+    if index_data[:name] && table_data[:name]
+      raise NoBrainer::Error::MissingIndex.new("Please run \"rake db:update_indexes\" to create the index `#{index_data[:name]}` " +
+                                               "in the table `#{table_data[:name]}`\n" +
                                                "--> Read http://nobrainer.io/docs/indexes for more information.")
     end
     raise
