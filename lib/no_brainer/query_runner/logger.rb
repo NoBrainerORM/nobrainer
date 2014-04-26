beff1ecc77aec73bafe567391ec3a12c1c34602c
Nicolas Viennot <nicolas@viennot.biz>
2014-04-26 00:55:44 -0400

    Refactor the logger

--- lib/no_brainer/query_runner/logger.rb
+++ lib/no_brainer/query_runner/logger.rb
@@ -13,23 +13,29 @@ class NoBrainer::QueryRunner::Logger < NoBrainer::QueryRunner::Middleware
     return unless NoBrainer.logger.debug?
 
     duration = Time.now - start_time
-    msg = env[:query].inspect.gsub(/\n/, '').gsub(/ +/, ' ')
 
-    msg = "(#{env[:db_name]}) #{msg}" if env[:db_name]
-    msg = "[#{(duration * 1000.0).round(1)}ms] #{msg}"
+    msg_duration = (duration * 1000.0).round(1).to_s
+    msg_duration = " " * [0, 5 - msg_duration.size].max + msg_duration
+    msg_duration = "[#{msg_duration}ms] "
+
+    msg_db = "[#{env[:db_name]}] " if env[:db_name]
+    msg_query = env[:query].inspect.gsub(/\n/, '').gsub(/ +/, ' ')
+    msg_exception = " #{exception.class} #{exception.message.split("\n").first}" if exception
+    msg_last = nil
 
     if NoBrainer::Config.colorize_logger
-      if exception
-        msg = "#{msg} \e[0;31m#{exception.class} #{exception.message.split("\n").first}\e[0m"
-      else
-        case NoBrainer::RQL.type_of(env[:query])
-        when :write      then msg = "\e[1;31m#{msg}\e[0m" # red
-        when :read       then msg = "\e[1;32m#{msg}\e[0m" # green
-        when :management then msg = "\e[1;33m#{msg}\e[0m" # yellow
-        end
-      end
+      query_color = case NoBrainer::RQL.type_of(env[:query])
+                    when :write      then "\e[1;31m" # red
+                    when :read       then "\e[1;32m" # green
+                    when :management then "\e[1;33m" # yellow
+                    end
+      msg_duration = [query_color, msg_duration].join
+      msg_db = ["\e[0;34m", msg_db, query_color].join if msg_db
+      msg_exception = ["\e[0;31m", msg_exception].join if msg_exception
+      msg_last = "\e[0m"
     end
 
+    msg = [msg_duration, msg_db, msg_query, msg_exception, msg_last].join
     NoBrainer.logger.debug(msg)
   end
 end
