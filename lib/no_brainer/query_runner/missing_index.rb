697947d9898e43c5bd40baac99c4d6dec6c664fb
Nicolas Viennot <nicolas@viennot.biz>
2014-04-25 23:51:18 -0400

    Use the new db error messages for auto table creation.

--- lib/no_brainer/query_runner/missing_index.rb
+++ lib/no_brainer/query_runner/missing_index.rb
@@ -2,30 +2,17 @@ class NoBrainer::QueryRunner::MissingIndex < NoBrainer::QueryRunner::Middleware
   def call(env)
     @runner.call(env)
   rescue RethinkDB::RqlRuntimeError => e
-    if e.message =~ /^Index `(.+)` was not found\.$/
+    if e.message =~ /^Index `(.+)` was not found on table `(.+)\.(.+)`\.$/
       index_name = $1
-      table_name = find_table_names(env[:query])
+      database_name = $2
+      table_name = $3
 
       err_msg  = "Please run \"rake db:update_indexes\" to create the index `#{index_name}`"
-      err_msg += " in the table #{table_name.map { |t| "`#{t}`"}.join(", ")}." if table_name.present?
+      err_msg += " in the table `#{database_name}.#{table_name}`."
       err_msg += "\n--> Read http://nobrainer.io/docs/indexes for more information."
 
       raise NoBrainer::Error::MissingIndex.new(err_msg)
     end
     raise
-  end
-
-  private
-
-  def find_table_names(terms)
-    terms = terms.body.args if terms.is_a?(RethinkDB::RQL)
-    terms.map do |term|
-      next unless term.is_a?(Term)
-      if term.type == Term::TermType::TABLE
-        term.args.first.datum.r_str
-      else
-        find_table_names(term.args)
-      end
-    end.flatten.uniq
   end
 end
--- lib/no_brainer/query_runner/table_on_demand.rb
+++ lib/no_brainer/query_runner/table_on_demand.rb
@@ -3,8 +3,8 @@ class NoBrainer::QueryRunner::TableOnDemand < NoBrainer::QueryRunner::Middleware
     @runner.call(env)
   rescue RuntimeError => e
     if NoBrainer::Config.auto_create_tables &&
-       e.message =~ /^Table `(.+)` does not exist\.$/
-      auto_create_table(env, $1)
+       e.message =~ /^Table `(.+)\.(.+)` does not exist\.$/
+      auto_create_table(env, $1, $2)
       retry
     end
     raise
@@ -12,33 +12,17 @@ class NoBrainer::QueryRunner::TableOnDemand < NoBrainer::QueryRunner::Middleware
 
   private
 
-  def auto_create_table(env, table_name)
-    if env[:auto_create_table] == table_name
-      raise "Auto table creation is not working with #{table_name}"
+  def auto_create_table(env, database_name, table_name)
+    if env[:auto_create_table] == [database_name, table_name]
+      raise "Auto table creation is not working for `#{database_name}.#{table_name}`"
     end
-    env[:auto_create_table] = table_name
+    env[:auto_create_table] = [database_name, table_name]
 
-    # FIXME This stinks.
-    database_names = find_db_names(env[:query])
-    case database_names.size
-    when 0 then NoBrainer.table_create(table_name)
-    when 1 then NoBrainer.with_database(database_names.first) { NoBrainer.table_create(table_name) }
-    else raise "Ambiguous database name for creation on demand: #{database_names}"
+    NoBrainer.with_database(database_name) do
+      NoBrainer.table_create(table_name)
     end
   rescue RuntimeError => e
     # We might have raced with another table create
-    raise unless e.message =~ /Table `#{table_name}` already exists/
-  end
-
-  def find_db_names(terms)
-    terms = terms.body.args if terms.is_a?(RethinkDB::RQL)
-    terms.map do |term|
-      next unless term.is_a?(Term)
-      if term.type == Term::TermType::DB
-        term.args.first.datum.r_str
-      else
-        find_db_names(term.args)
-      end
-    end.flatten.uniq
+    raise unless e.message =~ /Table `#{database_name}\.#{table_name}` already exists/
   end
 end
