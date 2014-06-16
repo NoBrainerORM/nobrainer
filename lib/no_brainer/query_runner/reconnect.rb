6b2a1c50a34da6537d987828506b58af55b7880d
Nicolas Viennot <nicolas@viennot.biz>
2014-06-15 23:14:45 -0400

    More helpful reconnection messages

--- lib/no_brainer/query_runner/reconnect.rb
+++ lib/no_brainer/query_runner/reconnect.rb
@@ -4,20 +4,20 @@ class NoBrainer::QueryRunner::Reconnect < NoBrainer::QueryRunner::Middleware
   rescue StandardError => e
     # TODO test that thing
     if is_connection_error_exception?(e)
-      retry if reconnect
+      retry if reconnect(e)
     end
     raise
   end
 
   private
 
-  def reconnect
+  def reconnect(e)
     # FIXME thread safety? perhaps we need to use a connection pool
     # XXX Possibly dangerous, as we could reexecute a non idempotent operation
     # Check the semantics of the db
     NoBrainer::Config.max_reconnection_tries.times do
       begin
-        NoBrainer.logger.try(:warn, "Lost connection to #{NoBrainer::Config.rethinkdb_url}, retrying...")
+        warn_reconnect(e)
         sleep 1
         NoBrainer.connection.reconnect(:noreply_wait => false)
         return true
@@ -41,5 +41,15 @@ class NoBrainer::QueryRunner::Reconnect < NoBrainer::QueryRunner::Middleware
     else
       false
     end
+  end
+
+  def warn_reconnect(e)
+    if e.is_a?(RethinkDB::RqlRuntimeError)
+      e_msg = e.message.split("\n").first
+      msg = "Server #{NoBrainer::Config.rethinkdb_url} not ready - #{e_msg}, retrying..."
+    else
+      msg = "Connection issue with #{NoBrainer::Config.rethinkdb_url} - #{e}, retrying..."
+    end
+    NoBrainer.logger.try(:warn, msg)
   end
 end
