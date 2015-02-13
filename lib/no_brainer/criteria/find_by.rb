80f51f20f71c9f50db4555a11fd305da3cca2333
Fadi Hadzh <fdhadzh@gmail.com>
2015-02-14 00:22:52 +0300

    change find_by to find_by? and find_by! to find_by

--- lib/no_brainer/criteria/find_by.rb
+++ lib/no_brainer/criteria/find_by.rb
@@ -1,11 +1,11 @@
 module NoBrainer::Criteria::FindBy
   extend ActiveSupport::Concern
 
-  def find_by(*block)
-    where(*block).first
+  def find_by?(*args, &block)
+    where(*args, &block).first
   end
 
-  def find_by!(*block)
-    find_by(*block).tap { |doc| raise NoBrainer::Error::DocumentNotFound unless doc }
+  def find_by(*args, &block)
+    find_by?(*args, &block).tap { |doc| raise NoBrainer::Error::DocumentNotFound unless doc }
   end
 end
