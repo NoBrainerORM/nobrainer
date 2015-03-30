d3117462c11f1d75281ca42b05b66ef0e6377c10
Jacob Hochstetler <Jacob.Hochstetler@fmr.com>
2015-03-30 15:04:18 -0500

    Added ability to Model.find(multiple_pks) supported by tests

--- lib/no_brainer/criteria/find.rb
+++ lib/no_brainer/criteria/find.rb
@@ -10,12 +10,44 @@ module NoBrainer::Criteria::Find
   end
   alias_method :find_by!, :find_by
 
-  def find?(pk)
-    without_ordering.find_by?(model.pk_name => pk)
+  def find?(*pks)
+    expects_array = pks.first.kind_of?(Array)
+    return pks.first if expects_array && pks.first.empty?
+    
+    pks = pks.flatten.compact.uniq
+    
+    case pks.size
+    when 0
+      nil
+    when 1
+      doc = without_ordering.find_by?(model.pk_name => pks.first)
+      expects_array ? [ doc ] : doc
+    else
+      find_some(pks)
+    end
   end
 
-  def find(pk)
-    without_ordering.find_by(model.pk_name => pk)
+  def find(*pks)
+    expects_array = pks.first.kind_of?(Array)
+    return pks.first if expects_array && pks.first.empty?
+    
+    pks = pks.flatten.compact.uniq
+    
+    case pks.size
+    when 0
+      raise_not_found(pks)
+    when 1
+      doc = without_ordering.find_by(model.pk_name => pks.first)
+      expects_array ? [ doc ] : doc
+    else
+      docs = find_some(pks)
+      if docs.count == pks.size
+       docs
+      else
+       missing_pks = pks - docs.map(&:pk_value)
+       raise_not_found([model.pk_name => missing_pks.join(', ')])
+      end
+    end
   end
   alias_method :find!, :find
 
@@ -23,5 +55,9 @@ module NoBrainer::Criteria::Find
 
   def raise_not_found(args)
     raise NoBrainer::Error::DocumentNotFound, "#{model} #{args.inspect.gsub(/\[{(.*)}\]/, '\1')} not found"
+  end
+  
+  def find_some(pks)
+    without_ordering.where({:or => pks.map {|pk| {model.pk_name => pk} } })
   end
 end
