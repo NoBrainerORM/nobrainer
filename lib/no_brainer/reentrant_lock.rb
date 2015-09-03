b758ec5224d397f2979c50b09f4d9c95dd2083c4
Nicolas Viennot <nicolas@viennot.biz>
2015-09-03 02:20:08 -0400

    Reentrant lock: Acquiring from the same instance an expired lock resets the counter

--- lib/no_brainer/reentrant_lock.rb
+++ lib/no_brainer/reentrant_lock.rb
@@ -7,11 +7,11 @@ class NoBrainer::ReentrantLock < NoBrainer::Lock
 
     result = NoBrainer.run do |r|
       selector.replace do |doc|
-        r.branch(doc[:instance_token].default(nil).eq(self.instance_token),
-                 doc.merge(:expires_at => self.expires_at,
-                           :lock_count => doc[:lock_count] + 1),
-                 r.branch(doc.eq(nil).or(doc[:expires_at] < r.now),
-                          self.attributes.merge(:lock_count => 1), doc))
+        r.branch(doc.eq(nil).or(doc[:expires_at] < r.now),
+                 self.attributes.merge(:lock_count => 1),
+                 r.branch(doc[:instance_token].default(nil).eq(self.instance_token),
+                          doc.merge(:expires_at => self.expires_at,
+                                    :lock_count => doc[:lock_count] + 1), doc))
       end
     end
 
