61d0f6dbd21ec4c2abae5fb730c87dab4c02d5a0
Nicolas Viennot <nicolas@viennot.biz>
2015-10-16 16:03:35 +0200

    Removing locking around sync_table_config
    Not so useful, and may cause problems (Related: #170)

--- lib/no_brainer/document/table_config/synchronizer.rb
+++ lib/no_brainer/document/table_config/synchronizer.rb
@@ -4,12 +4,7 @@ class NoBrainer::Document::TableConfig::Synchronizer
   end
 
   def sync_table_config(options={})
-    # XXX A bit funny since we might touch the lock table...
-    lock = NoBrainer::Lock.new('nobrainer:sync_table_config')
-
-    lock.synchronize do
-      @models.each(&:sync_table_config)
-    end
+    @models.each(&:sync_table_config)
 
     unless options[:wait] == false
       # Waiting on all models due to possible races
